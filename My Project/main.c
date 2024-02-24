cef48c1ea3bd7b9ec30a7fa350f8e5da26c63669
Finn Jonas Peper <j.jonas.peper@gmail.com>
2024-02-24 16:09:14 +0100

    2d array for all leds

--- My Project/main.c
+++ My Project/main.c
@@ -12,41 +12,39 @@ int main()
 	PORTA.PIN6CTRL = PORT_PULLUPEN_bm;
 	// PA6
 	
-	int port_a_outs[4]={
-		0b11111101,
-		0b11111011,
-		0b11101110,
-		0b11011110,
+	int port_a_b_outs[16][2]={
+		{0b11111101,0b00000100}, // LED 1
+		{0b11111011,0b00000100}, // LED 3
+		{0b11101110,0b00000100}, // LED 4
+		{0b11011110,0b00000100}, // LED 5
+		
+		{0b11011110,0b00001000}, // LED 5
+		{0b11101110,0b00001000}, // LED 6
+		{0b11111011,0b00001000}, // LED 7
+		{0b11111101,0b00001000}, // LED 8
+		
+		{0b11111101,0b00000001}, // LED 9
+		{0b11111011,0b00000001}, // LED 10
+		{0b11101110,0b00000001}, // LED 11
+		{0b11011110,0b00000001}, // LED 12
+		
+		{0b11011110,0b00000010}, // LED 13
+		{0b11101110,0b00000010}, // LED 14
+		{0b11111011,0b00000010}, // LED 15
+		{0b11111101,0b00000010}, // LED 16
+		
 	};
 	
-	int port_b_outs[4]={
-		0b00000001,
-		0b00000010,
-		0b00000100,
-		0b00001000,
-	};
+	int i;
 	
-	int i, j;
 	while(1){
 
-
-		for (i=0; i<=3; i++)
+		for (i=0; i<=15; i++)
 		{
-			PORTB.OUT = port_b_outs[i];
-			
-			for (j=0; j<=3; j++)
-			{
-				
-				PORTA.OUT =port_a_outs[j]; // Toggle state of pin 4
-				_delay_ms(150);
-			}
-
+			PORTA.OUT = port_a_b_outs[i][0];
+			PORTB.OUT = port_a_b_outs[i][1];
+			// Toggle state of pin 4
+			_delay_ms(50);
 		}
-		
-		
-		
-		
-
-
 	}
 }
