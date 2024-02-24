99328821e1a1e2891ff21c763201ea02c31ac0a3
Finn Jonas Peper <j.jonas.peper@gmail.com>
2024-02-24 10:34:09 +0100

    working blink with for loop

--- My Project/main.c
+++ My Project/main.c
@@ -12,14 +12,26 @@ int main()
 	PORTA.PIN6CTRL = PORT_PULLUPEN_bm;
 	// PA6
 	
+	int port_a_outs[4]={
+		0b1111101,
+		0b1111011,
+		0b1101111,
+		0b1011111
+	};
+	int i ;
 	while(1){
-		if(~PORTA.IN & PIN6_bm) /* check if PB2 is pulled to GND */
+
+		PORTB.OUT = 0b00000001; // all off
+		
+		
+		for (i=0; i<4; i++)
 		{
-			PORTB.OUTCLR = PIN1_bm; // Toggle state of pin 4
+			
+			PORTA.OUT =port_a_outs[i]; // Toggle state of pin 4
+			_delay_ms(120);
 		}
-		else{
-			PORTB.OUTSET = PIN1_bm; // all off
-		}
-		_delay_ms(1);
+
+
+
 	}
 }
