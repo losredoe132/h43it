c6c29c12f59178c0bab6e0f608ca3472c258fcea
Finn Jonas Peper <j.jonas.peper@gmail.com>
2024-02-28 17:41:39 +0100

    wokring debounce

--- GccApplication3/main.c
+++ GccApplication3/main.c
@@ -12,28 +12,28 @@
 #include <util/delay.h>
 
 
-
-int port_a_b_outs[16][2]={
+uint8_t port_a_b_outs[16][2]={
 	{0b11111101,0b00000100}, // LED 1
 	{0b11111011,0b00000100}, // LED 3
-	{0b11101110,0b00000100}, // LED 4
-	{0b11011110,0b00000100}, // LED 5
+	{0b11101111,0b00000100}, // LED 4
+	{0b11011111,0b00000100}, // LED 5
 	
-	{0b11011110,0b00001000}, // LED 5
-	{0b11101110,0b00001000}, // LED 6
+	{0b11011111,0b00001000}, // LED 5
+	{0b11101111,0b00001000}, // LED 6
 	{0b11111011,0b00001000}, // LED 7
 	{0b11111101,0b00001000}, // LED 8
 	
 	{0b11111101,0b00000001}, // LED 9
 	{0b11111011,0b00000001}, // LED 10
-	{0b11101110,0b00000001}, // LED 11
-	{0b11011110,0b00000001}, // LED 12
+	{0b11101111,0b00000001}, // LED 11
+	{0b11011111,0b00000001}, // LED 12
 	
-	{0b11011110,0b00000010}, // LED 13
-	{0b11101110,0b00000010}, // LED 14
+	{0b11011111,0b00000010}, // LED 13
+	{0b11101111,0b00000010}, // LED 14
 	{0b11111011,0b00000010}, // LED 15
 	{0b11111101,0b00000010}, // LED 16
-	{0b00000000,0b00000000}, // ALL OFF
+	
+	{0b11111111,0b00000000}, // ALL OFF
 	
 };
 
@@ -61,7 +61,7 @@ int main() {
 
 
 	// LED setup
-	RTC_init(100);
+	//RTC_init(100);
 	//
 	// 	PORTA.DIRSET=0b00000100;
 	// 	PORTB.DIRSET=0b00000001;
@@ -72,15 +72,19 @@ int main() {
 	// Button setup
 	PORTA.PIN6CTRL = PORT_ISC_FALLING_gc | PORT_PULLUPEN_bm; // Enable pull-up resistor
 
-	i=1;
-	x=200;
+	i;
+	x=0;
 
-	PORTA.OUT = 0b00000000;
-	PORTB.OUT = 0b00000001;
-	
 	sei(); // Set global interrupts
 	while(1){
-		
+		for (i=0; i<=15; i++){
+			if (i<=x){
+				
+				PORTA.OUT = port_a_b_outs[i][0];
+				PORTB.OUT= port_a_b_outs[i][1];
+				_delay_ms(1);
+			}
+		}
 
 		// Toggle state of pin 4
 		
@@ -90,20 +94,29 @@ int main() {
 
 }
 ISR(PORTA_PORT_vect) {
-
-	i++;
-	_delay_ms(100);
+PORTA.OUT = port_a_b_outs[16][0];
+PORTB.OUT= port_a_b_outs[16][1];
 	while (~PORTA.IN& btn_pin){
 
 	}
+	
+	
+	
+	x++;
+	if (x >15){
+		x=0;
+	}
+	_delay_ms(100);
 	PORTA.INTFLAGS |= btn_pin; // Clear interrupt flag
 
 }
 
-ISR(RTC_CNT_vect)
-{
-	RTC.INTFLAGS = RTC_OVF_bm;            // Clear flag by writing '1':
-	PORTA.OUT = port_a_b_outs[i][0];
-	PORTB.OUTTGL= port_a_b_outs[i][1];
-	
-}
+// ISR(RTC_CNT_vect)
+// {
+// 		PORTA.OUT = port_a_b_outs[i][0];
+// 		PORTB.OUTTGL= port_a_b_outs[i][1];
+// 	_delay_ms(1);
+
+// 	RTC.INTFLAGS = RTC_OVF_bm;            // Clear flag by writing '1':
+
+//}
