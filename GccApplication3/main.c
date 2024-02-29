8fb63a59cbbfeccda90f9e6464aab12be5afc465
Finn Jonas Peper <j.jonas.peper@gmail.com>
2024-02-29 20:26:37 +0100

    working PIT with POWER DOWN

--- GccApplication3/main.c
+++ GccApplication3/main.c
@@ -45,16 +45,40 @@ int j ;
 
 void RTCA_init(int RTCdelay)
 {
-	RTC.CLKSEL = RTC_CLKSEL_INT32K_gc;    // 32.768kHz Internal Crystal Oscillator (INT32K)
+	while (RTC.STATUS > 0) { /* Wait for all register to be synchronized */
+	}
 
-	while (RTC.STATUS > 0);               // Wait for all register to be synchronized
-	RTC.PER = RTCdelay;                   // Set period for delay
-	RTC.INTCTRL |= RTC_OVF_bm;            // Enable overflow Interrupt which will trigger ISR
-	RTC.CTRLA = RTC_PRESCALER_DIV32768_gc    // 32768 / 32 = 1024 (sec) ~ 1 ms
-	| RTC_RTCEN_bm                        // Enable: enabled
-	| RTC_RUNSTDBY_bm;                    // Run In Standby: enabled
+	RTC.CTRLA = RTC_PRESCALER_DIV32768_gc /* 32768 */
+	| 1 << RTC_RTCEN_bp       /* Enable: enabled */
+	| 0 << RTC_RUNSTDBY_bp;   /* Run In Standby: disabled */
+	
+}
+void RTC_PIT_init(){
+	RTC.CLKSEL = RTC_CLKSEL_INT1K_gc; 
+	while (RTC.PITSTATUS > 0) { /* Wait for all register to be synchronized */
+	}
+	RTC.PITINTCTRL = RTC_PI_bm;
+	RTC.PITCTRLA = RTC_PERIOD_CYC512_gc | RTC_PITEN_bm;
 }
 
+
+void TCA0_init(int TCAdelay)
+{
+	// enable overflow interrupt
+	TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;
+
+	// set Normal mode
+	TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_NORMAL_gc;
+
+	// disable event counting
+	TCA0.SINGLE.EVCTRL &= ~(TCA_SINGLE_CNTEI_bm);
+
+	// set the period
+	TCA0.SINGLE.PER = TCAdelay;
+
+	// set clock
+	TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1024_gc | TCA_SINGLE_ENABLE_bm;    /* source (sys_clk/8) +  start timer */
+}
 
 
 void LEDOnById(int i){
@@ -70,8 +94,9 @@ void allLEDoff(){
 int main() {
 
 
-	RTCA_init(10); // set periodic RTC triggering "awakening" delay in seconds
-
+	//RTCA_init(10); // set periodic RTC triggering "awakening" delay in seconds
+	TCA0_init(1);
+	RTC_PIT_init();
 	
 	PORTA.DIRSET = 0b10111111;
 	PORTB.DIRSET = 0b11111111;
@@ -80,34 +105,21 @@ int main() {
 	PORTA.PIN6CTRL = PORT_ISC_FALLING_gc | PORT_PULLUPEN_bm; // Enable pull-up resistor
 
 	x=8;
+	i=0;
 
 	sei(); // Set global interrupts
 	
-	
-
-	
 	while(1){
-		
 		// TODO AWAKEING Animation!
-
 		
-		for (int c=0; c<=STAY_ON_TIME*1000/RUN_TROUGH_SPEED/16; c++){
-			for (i=1; i<=16; i++){
-				if (i<=x){
-					
-					LEDOnById(i);
-				}
-				else{
-					allLEDoff();
-				}
-				_delay_ms(RUN_TROUGH_SPEED);
-			}
-		}
-
+		_delay_ms(1000);
+		// wait until button is released
 		allLEDoff();
-		set_sleep_mode(SLEEP_MODE_IDLE);
+		
+		SLPCTRL.CTRLA |= SLPCTRL_SMODE_PDOWN_gc; // set POWER DOWN as sleep mode
+		SLPCTRL.CTRLA |= SLPCTRL_SEN_bm; // enable sleep mode
+		
 		cli();
-		sleep_enable();
 		PORTA_INTFLAGS |= PORT_INT7_bm;
 		// Toggle state of pin 4
 		sei();
@@ -119,22 +131,23 @@ int main() {
 }
 ISR(PORTA_PORT_vect) {
 
-	_delay_ms(200);
-	// if button is still pressed
-	if (~PORTA.IN& btn_pin){
-		x++;
-		if (x >17){
-			x=16;
-		}
-	}
-	// wait until user released button
-	while (~PORTA.IN& btn_pin)
-	{
-	}
 	PORTA.INTFLAGS |= btn_pin; // Clear interrupt flag
 }
 
-ISR(RTC_CNT_vect)
+ISR(RTC_PIT_vect)
 {
-	RTC.INTFLAGS = RTC_OVF_bm;            // Clear flag by writing '1':
+	RTC.PITINTFLAGS = RTC_PI_bm;// Clear interrupt flag
+}
+
+ISR(TCA0_OVF_vect)
+{
+	i++;
+	
+	if (i<x){LEDOnById(i);}
+	else{allLEDoff();}
+	
+	if (i>16){i=0;}
+
+	// The interrupt flag has to be cleared manually
+	TCA0.SINGLE.INTFLAGS = TCA_SINGLE_OVF_bm;
 }
