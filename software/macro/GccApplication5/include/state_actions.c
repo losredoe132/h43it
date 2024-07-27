57af8d62bc5bab917582e2971436cb3f4899afea
Finn Jonas Peper <j.jonas.peper@gmail.com>
2024-07-27 20:28:48 +0200

    clean up FSM example definition

--- software/macro/GccApplication5/include/state_actions.c
+++ software/macro/GccApplication5/include/state_actions.c
@@ -1,10 +1,5 @@
 // State action functions
 
-void SHORTAction() {
-	LEDOnById(1);
-    printf("Entering state SHORT \n");
-}
-
 void LongAction() {
 	LEDOnById(1);
     printf("Entering state Long \n");
@@ -15,19 +10,9 @@ void IdleAction() {
     printf("Entering state Idle \n");
 }
 
-void IDLEAction() {
-	LEDOnById(1);
-    printf("Entering state IDLE \n");
-}
-
 void ShortAction() {
 	LEDOnById(1);
     printf("Entering state Short \n");
-}
-
-void LONGAction() {
-	LEDOnById(1);
-    printf("Entering state LONG \n");
 }
 
 
--- software/macro/GccApplication5/include/transition_table.c
+++ software/macro/GccApplication5/include/transition_table.c
@@ -1,45 +1,30 @@
 // Define States 
-State  SHORT ,   Long ,   Idle ,   IDLE ,   Short ,   LONG  ;
+State  Long ,   Idle ,   Short  ;
 
 // Define transitions for each state
 
-Transition SHORTTransitions[] = {
-    
-};
 Transition LongTransitions[] = {
     
-    { &IDLE, pred_button_released}  
+    { &Idle, pred_button_released}  
     
 };
 Transition IdleTransitions[] = {
     
-    { &SHORT, pred_button_short_push}  
-    
-};
-Transition IDLETransitions[] = {
+    { &Short, pred_button_short_push}  
     
 };
 Transition ShortTransitions[] = {
     
-    { &IDLE, pred_button_released} ,  
+    { &Idle, pred_button_released} ,  
     
-    { &LONG, pred_button_long_push}  
-    
-};
-Transition LONGTransitions[] = {
+    { &Long, pred_button_long_push}  
     
 };
 
 // Define states with transitions
 
-State SHORT = { "SHORT", SHORTAction, SHORTTransitions, 0 };
-
 State Long = { "Long", LongAction, LongTransitions, 1 };
 
 State Idle = { "Idle", IdleAction, IdleTransitions, 1 };
 
-State IDLE = { "IDLE", IDLEAction, IDLETransitions, 0 };
-
 State Short = { "Short", ShortAction, ShortTransitions, 2 };
-
-State LONG = { "LONG", LONGAction, LONGTransitions, 0 };
